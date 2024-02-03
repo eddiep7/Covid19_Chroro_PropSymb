0fbcbe04b545f8542234f89e773748315836e8e0
eddiep7 <146613466+eddiep7@users.noreply.github.com>
2024-02-03 13:43:06 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+mapboxgl.accessToken =
+    'pk.eyJ1Ijoia2F0ZXNhZSIsImEiOiJjbHMxNDdnbHUwODdsMmpwNWliYzZzOGdjIn0.eK5A1j3I8VWSkpZhnBV9EA';
 let map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/dark-v10',
@@ -6,67 +7,3 @@ let map = new mapboxgl.Map({
     minZoom: 4, // minimum zoom level of the map
     center: [138, 38] // starting center
 });
-
-const grades = [4, 5, 6],
-    colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)'],
-    radii = [5, 15, 20];
-
-map.on('load', () => {
-    map.addSource('covid-data', {
-        type: 'geojson',
-        data: 'assets/us-covid-2020-counts.geojson'
-    });
-
-    map.addLayer({
-        'id': 'covid-data-point',
-        'type': 'circle',
-        'source': 'covid-data',
-        'paint': {
-            'circle-radius': {
-                'property': 'your_property_for_radius', // Update with your property
-                'stops': [
-                    [grades[0], radii[0]],
-                    [grades[1], radii[1]],
-                    [grades[2], radii[2]]
-                ]
-            },
-            'circle-color': {
-                'property': 'your_property_for_color', // Update with your property
-                'stops': [
-                    [grades[0], colors[0]],
-                    [grades[1], colors[1]],
-                    [grades[2], colors[2]]
-                ]
-            },
-            'circle-stroke-color': 'white',
-            'circle-stroke-width': 1,
-            'circle-opacity': 0.6
-        }
-    });
-
-    map.on('click', 'covid-data-point', (event) => {
-        // Update with your popup content
-        new mapboxgl.Popup()
-            .setLngLat(event.features[0].geometry.coordinates)
-            .setHTML(`<strong>Magnitude:</strong> ${event.features[0].properties.your_property}`)
-            .addTo(map);
-    });
-});
-
-const legend = document.getElementById('legend');
-var labels = ['<strong>Magnitude</strong>'],
-    vbreak;
-
-for (var i = 0; i < grades.length; i++) {
-    vbreak = grades[i];
-    dot_radii = 2 * radii[i];
-    labels.push(
-        '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
-        'px; height: ' +
-        dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
-        '</span></p>');
-}
-
-const source =
-    '<p style="text-align: right; font-size:10pt">Source: <a href="https://data.census.gov/table/ACSDP5Y2018.DP05?g=0100000US$050000&d=ACS%205-Year%20Estimates%20Data%20Profiles&hidePreview=true</a></p>';
-legend.innerHTML = labels.join('') + source;
